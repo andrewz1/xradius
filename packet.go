82693ffbe2fa20f1cc3e9eb6e36add78d5e1e8c5
Andrey Safonov <andrey.safonov@gmail.com>
2019-11-10 05:04:06 +0300

    cleanup

--- packet.go
+++ packet.go
@@ -1,5 +1,10 @@
 package xradius
 
+import (
+	"encoding/binary"
+	"errors"
+)
+
 type RadiusCode byte
 
 // RFC constants
@@ -28,6 +33,9 @@ const (
 const (
 	PacketMinLen = 20   // Min packet len
 	PacketMaxLen = 4096 // Max packet len
+
+	AuthLen  = 16 // Auth data len
+	AttrsCap = 32 // Alloc space for attrs for append
 )
 
 type Packet struct {
@@ -38,7 +46,97 @@ type Packet struct {
 	attrs  []*Attr     // Attr slice
 	vids   []VendorID  // Vendor IDs form packet
 	secret []byte      // Radius shared secret
-	data   []byte      // Raw packet data
+	data   *rBuf       // Raw packet data
 	udata  interface{} // User data
 	reply  bool        // Is this reply
+}
+
+func ParsePacket(buf []byte) (pkt *Packet, err error) {
+	if len(buf) < PacketMinLen {
+		err = errors.New("Packet too short")
+		return
+	}
+	pLen := binary.BigEndian.Uint16(buf[2:])
+	if pLen < PacketMinLen || pLen > PacketMaxLen || int(pLen) > len(buf) {
+		err = errors.New("Packet len error")
+		return
+	}
+	pkt = &Packet{
+		attrs: make([]*Attr, 0, AttrsCap),
+		data:  rbGet(buf),
+	}
+	defer func() {
+		if err != nil {
+			for _, a := range pkt.attrs { // remove cross ref
+				a.pkt = nil
+			}
+			rbPut(pkt.data)
+		}
+	}()
+	v, _ := pkt.data.getByte()
+	pkt.code = RadiusCode(v)
+	pkt.id, _ = pkt.data.getByte()
+	pkt.auth, _ = pkt.data.getSlice(AuthLen)
+	vm := make(map[VendorID]struct{})
+	for pkt.data.left() >= 2 {
+		var (
+			t, l, vt, vl byte
+			v, vv        []byte
+			vid          uint32
+		)
+		if t, l, v, err = pkt.data.getAttr(); err != nil {
+			return
+		}
+		if AttrType(t) != AttrVSA {
+			ad := GetAttrByAttr(AttrType(t))
+			tag := byte(0)
+			if ad.IsTagged() && len(v) > 0 {
+				tag = v[0]
+				v = v[1:]
+			}
+			a := &Attr{
+				atype: AttrType(t),
+				alen:  l,
+				tag:   tag,
+				data:  v,
+				ad:    ad,
+				pkt:   pkt,
+			}
+			pkt.attrs = append(pkt.attrs, a)
+		} else {
+			rb := rbNew(v)
+			if vid, err = rb.getUInt32(); err != nil {
+				return
+			}
+			vm[VendorID(vid)] = struct{}{}
+			for rb.left() >= 2 {
+				if vt, vl, vv, err = rb.getAttr(); err != nil {
+					return
+				}
+				ad := GetVSAByAttr(VendorID(vid), VendorType(vt))
+				tag := byte(0)
+				if ad.IsTagged() && len(vv) > 0 {
+					tag = vv[0]
+					vv = vv[1:]
+				}
+				a := &Attr{
+					atype: AttrVSA,
+					alen:  vl + 6,
+					vid:   VendorID(vid),
+					vtype: VendorType(vt),
+					vlen:  vl,
+					tag:   tag,
+					data:  vv,
+					ad:    ad,
+					pkt:   pkt,
+				}
+				pkt.attrs = append(pkt.attrs, a)
+			}
+		}
+	}
+	pkt.vids = make([]VendorID, 0, len(vm))
+	for k := range vm {
+		pkt.vids = append(pkt.vids, k)
+	}
+	return
 }
---
+++ rbuf.go
@@ -0,0 +1,126 @@
+package xradius
+
+import (
+	"encoding/binary"
+	"errors"
+	"sync"
+)
+
+type rBuf struct {
+	bb []byte // buffer data
+	bp int    // buffer pointer
+	bl int    // buffer left
+}
+
+var (
+	errNoData      = errors.New("no data")
+	errInvalidData = errors.New("invalid attr data")
+	rbPool         sync.Pool
+)
+
+func rbGet(src []byte) (rb *rBuf) {
+	if v := rbPool.Get(); v != nil {
+		rb = v.(*rBuf)
+		if cap(rb.bb) < len(src) { // resize buffer
+			rb.bb = make([]byte, len(src))
+		} else {
+			rb.bb = rb.bb[:len(src)]
+		}
+		rb.bl = len(src)
+	} else {
+		rb = &rBuf{
+			bb: make([]byte, len(src)),
+			bl: len(src),
+		}
+	}
+	copy(rb.bb, src)
+	return
+}
+
+func rbPut(rb *rBuf) {
+	rb.bb = rb.bb[:0]
+	rb.bp = 0
+	rb.bl = 0
+	rbPool.Put(rb)
+}
+
+func rbNew(src []byte) *rBuf {
+	return &rBuf{
+		bb: src,
+		bl: len(src),
+	}
+}
+
+func (rb *rBuf) left() int {
+	return rb.bl
+}
+
+func (rb *rBuf) getSlice(ln int) (out []byte, err error) {
+	if rb.bl < ln {
+		err = errNoData
+		return
+	}
+	out = rb.bb[rb.bp : rb.bp+ln]
+	rb.bp += ln
+	rb.bl -= ln
+	return
+}
+
+func (rb *rBuf) getByte() (out byte, err error) {
+	if rb.bl < 1 {
+		err = errNoData
+		return
+	}
+	out = rb.bb[rb.bp]
+	rb.bp++
+	rb.bl--
+	return
+}
+
+func (rb *rBuf) getUInt16() (out uint16, err error) {
+	if rb.bl < 2 {
+		err = errNoData
+		return
+	}
+	out = binary.BigEndian.Uint16(rb.bb[rb.bp:])
+	rb.bp += 2
+	rb.bl -= 2
+	return
+}
+
+func (rb *rBuf) getUInt32() (out uint32, err error) {
+	if rb.bl < 4 {
+		err = errNoData
+		return
+	}
+	out = binary.BigEndian.Uint32(rb.bb[rb.bp:])
+	rb.bp += 4
+	rb.bl -= 4
+	return
+}
+
+func (rb *rBuf) getAttr() (t, l byte, v []byte, err error) {
+	bp := rb.bp
+	bl := rb.bl
+	defer func() {
+		if err != nil { // restore buffer state on error
+			rb.bp = bp
+			rb.bl = bl
+		}
+	}()
+	if t, err = rb.getByte(); err != nil {
+		return
+	}
+	if l, err = rb.getByte(); err != nil {
+		return
+	}
+	sl := int(l) - 2
+	if sl < 0 {
+		err = errInvalidData
+		return
+	} else if sl == 0 {
+		return
+	}
+	v, err = rb.getSlice(sl)
+	return
+}
